57ba9b6e45d6ea0e6bcee6521ad2a54eea24f7fb
obnol <longboqiu@hotmail.com>
2022-04-24 17:38:32 +0200

    Added signup and login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LogIn from './app/landing/LogIn';
 import { LinkedInCallback } from 'react-linkedin-login-oauth2';
+import LogIn from './app/auth/LogIn';
+import Signup from './app/auth/SignUp';
 
 const Home = () => {
   return <div>Home</div>;
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route exact path='/' element={<Home />} />
       <Route path='/login' element={<LogIn />} />
+      <Route path='/signup' element={<Signup />} />
       <Route exact path='/linkedin' element={<LinkedInCallback />} />
     </Routes>
   );
