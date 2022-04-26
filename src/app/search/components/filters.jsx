be7b1bf71ee1a3879b24e81e14388cc29ce8ab5a
pere.masip <pere.masip@estudiantat.upc.edu>
2022-04-26 17:48:32 +0200

    radio buttons WIP

--- src/app/search/components/filters.jsx
+++ src/app/search/components/filters.jsx
@@ -1,6 +1,7 @@
-import { Button } from "@material-ui/core";
+import { Button, Checkbox, FormControlLabel, FormGroup, RadioGroup, Radio } from "@material-ui/core";
 import { render } from "@testing-library/react";
 import { root } from "postcss";
+
 import React, { useState } from "react";
 import { FiFilter } from 'react-icons/fi';
 
@@ -32,15 +33,37 @@ const Filters = () => {
                 {
                     display ? (
                         <>
-                        <div className='flex h-full w-full justify-center items-center'>
-                            <p className='font-medium text-sm text-black'>Distancia</p>
-                        </div>
-                        <div className='flex h-full w-full justify-center items-center'>
-                            <p className='font-medium text-sm text-black'>Duración</p>
-                        </div>
-                        <div className='flex h-full w-full justify-center items-center'>
-                            <p className='font-medium text-sm text-black'>Categoría</p>
-                        </div>
+                            <div className='flex h-full w-full justify-center items-center'>
+                                <p className='font-medium text-sm text-black'>Distancia</p>
+                            </div>
+                            <div class="flex justify-center">
+                                <div>
+                                    <div class="form-check">
+                                        <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                                            <label class="form-check-label inline-block font-medium text-sm text-black" for="flexRadioDefault1">
+                                                Menos de 10 kilómetros
+                                            </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
+                                            <label class="form-check-label inline-block font-medium text-sm text-black" for="flexRadioDefault2">
+                                                Menos de 100 kilómetros
+                                            </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
+                                            <label class="form-check-label inline-block font-medium text-sm text-black" for="flexRadioDefault2">
+                                                Más de 100 kilómetros
+                                            </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex h-full w-full justify-center items-center'>
+                                <p className='font-medium text-sm text-black'>Duración</p>
+                            </div>
+                            <div className='flex h-full w-full justify-center items-center'>
+                                <p className='font-medium text-sm text-black'>Categoría</p>
+                            </div>
                         </>
                     ) : (
                         <></>
