856683a488054e0ed90c5ed04c0c3cb6abb52d98
obnol <longboqiu@hotmail.com>
2022-04-26 17:38:26 +0200

    updated category height

--- src/app/home/components/PopularCategories.jsx
+++ src/app/home/components/PopularCategories.jsx
@@ -3,21 +3,29 @@ import React from 'react';
 const Item = ({ item }) => {
   return (
     <div className='flex flex-col shrink-0 w-44'>
-      <img className='h-5/6' src={item.image} alt='image' />
-      <p className='h-1/6 text-base font-light'>{item.title}</p>
+      <img className='h-4/6' src={item.image} alt='image' />
+      <p className='h-2/6 text-base font-light'>{item.title}</p>
     </div>
   );
 };
 
 const PopularCategories = ({ categories }) => {
   return (
-    <div className='flex flex-col mx-5 mt-5'>
-      <div className='flex flex-row h-auto overflow-x-auto space-x-8'>
-        {categories.map((item, index) => (
-          <Item key={index} item={item} />
-        ))}
+    <>
+      <div className='flex flex-row justify-between mx-5 mt-5'>
+        <p className='flex font-medium w-3/4 text-xl'>
+          Categorias populares en nuestra plataforma
+        </p>
+        <p className='text-sm font-light'>ver más</p>
       </div>
-    </div>
+      <div className='flex flex-col mx-5 mt-5 h-auto'>
+        <div className='flex flex-row overflow-x-auto space-x-8'>
+          {categories.map((item, index) => (
+            <Item key={index} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
