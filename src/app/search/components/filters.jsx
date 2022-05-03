75889660cac83ed5f90ee998e8e85edfff7135b9
pere.masip <pere.masip@estudiantat.upc.edu>
2022-05-03 11:41:48 +0200

    added onChange functions

--- src/app/search/components/filters.jsx
+++ src/app/search/components/filters.jsx
@@ -7,6 +7,7 @@ import { FiFilter } from 'react-icons/fi';
 
 var opened = false;
 
+
 const displayHideFilters = (display) => {
     display ? (
         <div className='flex h-full w-full justify-center items-center'>
@@ -19,16 +20,44 @@ const displayHideFilters = (display) => {
     )
 }
 
+const submitValues =
+{
+    distance: 0,
+    duration: 0,
+    category: []
+};
+
+const onValueChange = (event) => {
+    if (event.target.id == "distance") {
+        submitValues.distance = event.target.value;
+        console.log(submitValues.distance);
+    }
+    else if (event.target.id == "duration") {
+        submitValues.duration = event.target.value;
+    }
+    else if (event.target.id == "category") {
+
+    }
+};
+
+const onSubmit = (event) => {
+    if (event.target.id == "distance")
+        submitValues.distance = event.target.value;
+};
+
 const Filters = () => {
     const [display, setDisplay] = useState(true);
+
     return (
         <>
-            <div className='flex mt-5 mx-5 flex-row justify-between'>
-                <p>Filtrar</p>
-                <Button variant="contained" onClick={() => setDisplay(!display)}>
+            <div
+                className='flex h-8 w-24 bg-blue-500 justify-center items-center rounded-lg'
+                onClick={() => setDisplay(!display)}
+            >
+                <div className='flex h-full w-full justify-center items-center'>
                     <FiFilter />
-                </Button>
-            </div>
+                </div>
+            </div>            
             <div>
                 {
                     display ? (
@@ -36,34 +65,59 @@ const Filters = () => {
                             <div className='flex h-full w-full justify-center items-center'>
                                 <p className='font-medium text-sm text-black'>Distancia</p>
                             </div>
-                            <div class="flex justify-center">
-                                <div>
-                                    <div class="form-check">
-                                        <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-                                            <label class="form-check-label inline-block font-medium text-sm text-black" for="flexRadioDefault1">
-                                                Menos de 10 kilómetros
-                                            </label>
+                            <div className="flex justify-center">
+                                <div onSubmit={onSubmit}>
+                                    <div className="form-check" onChange={onValueChange}>
+                                        <input className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" value="<10" id="distance" />
+                                        <label className="form-check-label inline-block font-medium text-sm text-black">
+                                            Menos de 10 kilómetros
+                                        </label>
                                     </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
-                                            <label class="form-check-label inline-block font-medium text-sm text-black" for="flexRadioDefault2">
-                                                Menos de 100 kilómetros
-                                            </label>
+                                    <div className="form-check" onChange={onValueChange}>
+                                        <input className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" value="<100" id="distance" />
+                                        <label className="form-check-label inline-block font-medium text-sm text-black">
+                                            Menos de 100 kilómetros
+                                        </label>
                                     </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
-                                            <label class="form-check-label inline-block font-medium text-sm text-black" for="flexRadioDefault2">
-                                                Más de 100 kilómetros
-                                            </label>
+                                    <div className="form-check" onChange={onValueChange}>
+                                        <input className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" value=">100" id="distance" />
+                                        <label className="form-check-label inline-block font-medium text-sm text-black">
+                                            Más de 100 kilómetros
+                                        </label>
                                     </div>
                                 </div>
                             </div>
                             <div className='flex h-full w-full justify-center items-center'>
                                 <p className='font-medium text-sm text-black'>Duración</p>
                             </div>
+                            <div className="flex justify-center">
+                                <div onSubmit={onSubmit}>
+                                    <div className="form-check" onChange={onValueChange}>
+                                        <input className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" value="1 mes" id="duration" />
+                                        <label className="form-check-label inline-block font-medium text-sm text-black">
+                                            1 mes
+                                        </label>
+                                    </div>
+                                    <div className="form-check" onChange={onValueChange}>
+                                        <input className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" value="1 semestre" id="duration" />
+                                        <label className="form-check-label inline-block font-medium text-sm text-black">
+                                            6 meses
+                                        </label>
+                                    </div>
+                                    <div className="form-check" onChange={onValueChange}>
+                                        <input className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" value="1 año" id="duration" />
+                                        <label className="form-check-label inline-block font-medium text-sm text-black">
+                                            1 año o más
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
                             <div className='flex h-full w-full justify-center items-center'>
                                 <p className='font-medium text-sm text-black'>Categoría</p>
                             </div>
+                            <button className="btn btn-default" type="submit">
+                                Aplicar Cambios
+                            </button>
                         </>
                     ) : (
                         <></>
